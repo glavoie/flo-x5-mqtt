a66f32ae0852f6faafd5819f8c4b1ac06aeaef5e
Gabriel Lavoie <glavoie@gmail.com>
2023-12-30 11:02:10 -0500

    Add types and validation.

--- flo_client/client.py
+++ flo_client/client.py
@@ -10,13 +10,13 @@ from flo_client.consts import *
 
 
 class FloX5Client:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str) -> None:
         self.next_refresh = datetime.now()
         self._auth = Auth(username, password)
 
         self._refresh()
 
-    def _refresh(self):
+    def _refresh(self) -> None:
         # Refresh every minutes at most
         if datetime.now() < self.next_refresh:
             return
@@ -26,7 +26,7 @@ class FloX5Client:
 
         self.next_refresh = datetime.now() + timedelta(seconds=REFRESH_DELAY_SECS)
 
-    def _get_stations(self):
+    def _get_stations(self) -> dict:
         resp = requests.get(STATIONS_URL, headers=self._get_headers())
         if resp.status_code != 200:
             raise Exception("Error getting stations.", resp.status_code, resp.text)
@@ -36,19 +36,19 @@ class FloX5Client:
 
         return resp.json()
 
-    def _get_sessions(self):
+    def _get_sessions(self) -> dict:
         resp = requests.get(SESSIONS_URL, headers=self._get_headers())
         if resp.status_code != 200:
             raise Exception("Error getting sessions.", resp.status_code, resp.text)
         return resp.json()
 
-    def _get_headers(self):
+    def _get_headers(self) -> dict:
         return {
             "Accept": "*/*",
             "Authorization": "Bearer " + self._auth.get_access_token(),
         }
 
-    def get_station_by_name(self, name: str):
+    def get_station_by_name(self, name: str) -> dict | None:
         self._refresh()
         for station in self._stations:
             if station["information"]["name"] == name:
@@ -56,7 +56,7 @@ class FloX5Client:
 
         return None
 
-    def get_session_by_id(self, id: str):
+    def get_session_by_id(self, id: str) -> dict | None:
         self._refresh()
         for session in self._sessions:
             if session["station"]["id"] == id:
@@ -64,7 +64,7 @@ class FloX5Client:
 
         return None
 
-    def is_station_online(self, station):
+    def is_station_online(self, station: dict | None) -> bool:
         if station is None:
             return False
 
@@ -73,7 +73,7 @@ class FloX5Client:
             or station[STATUS_KEY][STATE_KEY] == STATE_INUSE
         )
 
-    def is_vehicle_connected(self, station):
+    def is_vehicle_connected(self, station: dict | None) -> bool:
         if station is None:
             return False
 
@@ -82,7 +82,7 @@ class FloX5Client:
             or station[STATUS_KEY][PILOT_STATE_KEY] == PILOT_STATE_CHARGING
         )
 
-    def is_vehicle_charging(self, station):
+    def is_vehicle_charging(self, station: dict | None) -> bool:
         if station is None:
             return False
 
